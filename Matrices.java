6cba4876353c4b390c42e04a91ce15fd40c641f9
aki0923 <abibilaevaknazar34@gmail.com>
2024-11-29 15:52:22 +0600

    upload file Matrices.java

---
+++ Matrices.java
@@ -0,0 +1,28 @@
+package myProjects;
+import java.util.Scanner;
+public class Matrices {
+    static Scanner sc = new Scanner(System.in);
+    static String username = sc.nextLine();
+
+
+    public static void main(String[] args) {
+        int rowsNumbers = 7;
+        int columnsNumbers = 7;
+        String [][] map = new String[rowsNumbers][columnsNumbers];
+        map[0][0] = "A1"; map[0][1] = "A2"; map[0][2] = "A3"; map[0][3] = "A4"; map[0][4] = "A5"; map[0][5] = "A6"; map[0][6] = "A7";
+        map[1][0] = "B1"; map[1][1] = "B2"; map[1][2] = "B3"; map[1][3] = "B4"; map[1][4] = "B5"; map[1][5] = "B6"; map[1][6] = "B7";
+        map[2][0] = "C1"; map[2][1] = "C2"; map[2][2] = "C3"; map[2][3] = "C4"; map[2][4] = "C5"; map[2][5] = "C6"; map[2][6] = "C7";
+        map[3][0] = "D1"; map[3][1] = "D2"; map[3][2] = "D3"; map[3][3] = "D4"; map[3][4] = "D5"; map[3][5] = "D6"; map[3][6] = "D7";
+        map[4][0] = "E1"; map[4][1] = "E2"; map[4][2] = "E3"; map[4][3] = "E4"; map[4][4] = "E5"; map[4][5] = "E6"; map[4][6] = "E7";
+        map[5][0] = "F1"; map[5][1] = "F2"; map[5][2] = "F3"; map[5][3] = "F4"; map[5][4] = "F5"; map[5][5] = "F6"; map[5][6] = "F7";
+        map[6][0] = "G1"; map[6][1] = "G2"; map[6][2] = "G3"; map[6][3] = "G4"; map[6][4] = "G5"; map[6][5] = "G6"; map[6][6] = "G7";
+
+
+
+
+
+
+
+
+    }
+}
